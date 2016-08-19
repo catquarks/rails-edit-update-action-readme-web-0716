d99e4d34e4318a42b5aac275b9d441ba355438dd
Tami <catquarks@gmail.com>
2016-08-18 23:50:27 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+
+
+Post.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence)
+Post.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence)
+Post.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence)
+Post.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence)
+Post.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence)
+Post.create(title: Faker::Hipster.word, description: Faker::Hipster.sentence)
